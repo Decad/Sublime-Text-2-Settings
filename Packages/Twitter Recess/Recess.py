962f6986f73b8a5011c725b43c2b1f55135ff665
Declan cook <declancook44@gmail.com>
2013-01-07 19:03:45 +0000

    Initial Commit on mac

---
+++ Packages/Twitter Recess/Recess.py
@@ -0,0 +1,19 @@
+import sublime
+import sublime_plugin
+
+
+class RecessCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command('set_build_system', {
+          'file': 'Packages/Twitter Recess/Recess.sublime-build'
+        })
+        self.window.run_command('build')
+
+
+class RecessCompileCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command('set_build_system', {
+          'file': 'Packages/Twitter Recess/RecessCompile.sublime-build'
+        })
+        self.window.run_command('build')
+        sublime.active_window().run_command('revert')
